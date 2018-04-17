2bdf444c85d5eb40acfc6058156db2594ea2b398
kwiecien <k.d.kwiecinski@gmail.com>
2018-04-17 23:13:42 +0200

    feat: Fetch data with Volley

--- app/src/main/java/com/kk/bakingapp/ui/MainActivity.java
+++ app/src/main/java/com/kk/bakingapp/ui/MainActivity.java
@@ -3,8 +3,17 @@ package com.kk.bakingapp.ui;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.kk.bakingapp.R;
+
+import org.json.JSONArray;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +23,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setToolbar();
+
+        testVolley();
+
+    }
+
+    private void testVolley() {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json";
+
+        JsonArrayRequest jsonObjectRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d(MainActivity.class.getSimpleName(), response.toString()); // TODO handle response
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                    }
+                });
+        queue.add(jsonObjectRequest);
     }
 
     private void setToolbar() {
