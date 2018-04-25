ce735bcc9408013635edcac04b7e22b5f4b10260
kwiecien <k.d.kwiecinski@gmail.com>
2018-04-25 18:48:28 +0200

    feat: Add pending intent

--- app/src/main/java/com/kk/bakingapp/RecipeWidgetProvider.java
+++ app/src/main/java/com/kk/bakingapp/RecipeWidgetProvider.java
@@ -1,9 +1,13 @@
 package com.kk.bakingapp;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
+
+import com.kk.bakingapp.ui.MainActivity;
 
 /**
  * Implementation of App Widget functionality.
@@ -17,6 +21,12 @@ public class RecipeWidgetProvider extends AppWidgetProvider {
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.recipe_widget_provider);
         views.setTextViewText(R.id.appwidget_text, widgetText);
+
+        // Create an Intent to launch Application
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent =
+                PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        views.setOnClickPendingIntent(R.id.widget_layout, pendingIntent);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
