38fb2060b02a6561317022d6cf0acd20a60271b1
kwiecien <k.d.kwiecinski@gmail.com>
2018-05-09 22:14:52 +0200

    feat: Add RecipeDetail and RecipeStep test classes

---
+++ app/src/androidTest/java/com/kk/bakingapp/ui/RecipeStepFragmentTest.java
@@ -0,0 +1,16 @@
+package com.kk.bakingapp.ui;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class RecipeStepFragmentTest {
+
+    @Test
+    public void test() {
+        // TODO provide sensible implementation
+    }
+
+}
